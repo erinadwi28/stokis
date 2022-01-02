c50a9d54e8299b7f8a104a4a3477cfd349bb40d1
rahmau <rahmamaulani26@gmail.com>
2022-01-02 16:52:17 +0700

    rahma

---
+++ stokis.cpp
@@ -0,0 +1,292 @@
+#include<iostream>
+#include<conio.h>
+#include<stdio.h>
+#include<windows.h>
+#include<cstdlib>
+#include <string.h>
+
+using namespace std;
+
+struct item {
+	char nama_brg[50][50];
+	string kode_brg[20];
+	int stok[20];
+}itm;
+
+int z;
+
+//sampai di fitur tambah ini
+void tambah(){
+	system("cls");
+	int pilih, jml_tambah;
+	string kode;
+	int total_stok=0;
+	
+	cout<<"1. Tambah barang baru";
+	cout<<"\n2. Isi stok barang lama";
+	cout<<"\n\nPilih nomor\t: ";
+	cin>>pilih;
+	
+	switch(pilih){
+	case 1:
+		cout<<"\nMasukkan kode barang\t: ";
+    	cin>>itm.kode_brg[z];
+    	cout<<"Masukkan nama barang\t: ";
+    	cin>>itm.nama_brg[z];
+    	cout<<"Masukkan stok barang\t: ";
+    	cin>>itm.stok[z];
+    	z++;
+    	break;
+	
+	case 2:
+		cout<<"\nMasukkan kode barang\t: ";
+    	cin>>kode;
+    	
+    	for(int j=0;j<z;j++)
+    	{
+    		if (itm.kode_brg[j]==kode)
+      		{
+        		cout<<"\nNama barang\t\t: "<<itm.nama_brg[j];
+        		cout<<"\nStok barang saat ini\t: "<<itm.stok[j];
+        		cout<<"\n\nJumlah barang yang ingin ditambahkan\t:";
+        		cin>>jml_tambah;
+        		total_stok+=itm.stok[j];
+        		itm.stok[j] = total_stok + jml_tambah;
+        		cout<<"\nPenambahan stok berhasil";
+        		getch();
+      		} 
+			else 
+			{
+				cout<<"Kode yang Anda masukkan salah";
+				getch();
+			}
+    	}
+    	break;
+    	
+	default : 
+	cout<<"\nPilihan tidak ada\nMasukan pilihan sesuai dengan daftar"<<endl;
+	getch();
+    break;
+	}
+}
+
+void ambil(){
+	char a;
+	string i;
+	int j;
+	int jml_ambil;
+	int total_stok=0;
+	system("cls");
+	
+	cout<<"\nMasukkan kode barang\t: ";
+    cin>>i;
+    for(j=0;j<z;j++)
+    {
+    	if (itm.kode_brg[j]==i)
+      	{
+        	if(itm.stok[j]==0)
+        	{
+          		cout<<"\nMaaf barang kosong.";
+          		cout<<"\nApakah mau ambil barang yang lain? y\\n"<<endl;
+          		cin>>a;
+          		if (a=='y')
+          		{
+        			cout<<"\nMasukkan kode barang\t: ";
+        			cin>>i;
+        			j=-1;
+          		}
+          		else if (a=='Y')
+          		{
+        			cout<<"\nMasukkan kode barang:";
+        			cin>>i;
+        			j=-1;
+          		}
+          		else
+          		{
+        			if (total_stok!=0)
+        			{
+          				cout<<"\nStok barang yang ada\t: ";
+          				cout<<total_stok<<"item"<<"\nTerimakasih :) .";
+        			}
+        			break;
+        		}
+        	}
+        	else
+        	{
+        		cout<<"\nNama barang\t\t: "<<itm.nama_brg[j];
+        		cout<<"\nStok barang saat ini\t: "<<itm.stok[j];
+        		cout<<"\n\nJumlah barang yang ingin di ambil\t:";
+        		cin>>jml_ambil;
+        		total_stok+=itm.stok[j];
+        		itm.stok[j] = total_stok - jml_ambil;
+        		cout<<"\nApakah mau ambil barang yang lain? y\\n"<<endl;
+        		cin>>a;
+        		if (a=='y')
+        		{
+        			cout<<"\nMasukkan kode barang\t: ";
+        			cin>>i;
+        			j=-1;
+          		}
+        		else if (a=='Y')
+        		{
+        			cout<<"\nMasukkan kode barang\t: ";
+        			cin>>i;
+        			j=-1;
+          		}
+          		else
+          		{  
+			  		system("cls");
+			  		cout<<"Pegambilan Barang Berhasil\n";
+        			cout<<"\nStok barang "<<itm.nama_brg[j]<<" saat ini adalah "<<itm.stok[j];
+        			cout<<"\nTerimakasih :)";
+        			break;
+          		}
+        	}
+      	}
+    }
+    getch();
+}
+
+void tampil()
+{
+    int i, j=1;
+    system("cls");
+
+       for(i=0;i<z;i++)
+      {
+		  cout<<"\nKode barang\t: "<<itm.kode_brg[i];
+    	cout<<"\nNama barang\t: "<<itm.nama_brg[i];
+    	cout<<"\nStok barang\t: "<<itm.stok[i]<<endl<<endl;
+        j++;
+       }
+        getch();
+      }
+      
+void urut(){
+
+	int y, x, a, n, m;
+	string temp[40];
+		if (z>1){
+				for (y=1; y<=(z-1); y++){
+					a=(strcmp(itm.nama_brg[z], itm.nama_brg[y]));
+					if (a<=0){
+						strcpy (temp, itm.nama_brg[x]);
+						for (n=(z-1); n>=y; n--){
+							m=(n+1);
+							strcpy (itm.nama_brg[m], itm.nama_brg[n]);
+						}
+						strcpy(itm.nama_brg[y], temp);
+					}
+				}
+			}
+		}
+		cout<<"\nSetelah diurutkan dengan Metode Ascending: "<<endl;
+		for (z=1; z<=jml; z++){
+			cout<<x<<"z"<<itm.nama_brg[z];
+			cout<<endl;
+		}
+		cout<<endl;
+		
+		getch();
+}
+int main ()
+
+  void cari(){
+    int a, i, j,count;
+    string cari;
+    cout<<"Masukkan nama barang yang dicari: ";
+    cin>>cari;
+
+     //count+=z;
+    for(i=0;i<z;i++)
+      if(itm.nama_brg[i] == cari){
+		 cout<<"Data ditemukan"<<z;	 
+      }
+    getch();
+  }
+
+  void urut(){
+  	int jml, pilih, x, y, a, m, n;
+	char temp[40];
+		
+		cout<<"Sebelum Urut: "; tampil();
+			if (z>1){
+				for (y=1; y<=(z-1); y++){
+					a=(strcmp(itm.nama_brg[z], itm.nama_brg[y]));
+					if (a<=0){
+						strcpy (temp, itm.nama_brg[z]);
+						for (n=(z-1); n>=y; n--){
+							m=(n+1);
+							strcpy (itm.nama_brg[m], itm.nama_brg[n]);
+						}
+						strcpy(itm.nama_brg[y], temp);
+					}
+				}
+			}
+		cout<<"\nSetelah diurutkan dengan Metode Ascending: "<<endl;
+		for (x=0; x<z; x++){
+			cout<<x<<". "<<itm.nama_brg[x];
+			cout<<endl;
+		}
+		cout<<endl;
+		
+		getch();
+ }
+      
+int main (){
+    int i;
+    system("cls");
+
+    while(1)
+    {
+      cout<<"\nSelamat Datang di Stokis"<<endl;
+      cout<<"\n1: Tambah data barang";
+      cout<<"\n2: Tampil data barang";
+      cout<<"\n3: Ambil barang";
+      cout<<"\n4: Cari data barang";
+      cout<<"\n5: Urutkan data barang";
+      cout<<"\n6: Keluar"<<endl;
+      cout<<"\n\n Pilih nomor\t: ";
+      cin>>i;
+
+      switch(i)
+      {
+        case 1:
+        {
+          tambah();
+          break;
+        }
+        case 2:
+        {
+          tampil();
+          break;
+        }
+        case 3:
+        {
+          ambil();
+          break;
+        }
+        case 4:
+        {
+          cari();
+          break;
+        }
+
+           case 5:
+        {
+          urut();
+          break;
+        }
+         case 6:
+        {
+      	exit(0);
+          break;
+        }
+      }
+      if (i==6)
+        break;
+      	system("cls");
+    }
+    getch();
+    return 0;
+}
